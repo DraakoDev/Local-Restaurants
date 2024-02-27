672c0a9afce76f9dac4bc3e61e78695ec2094aa7
Draakox <draakox.dev@gmail.com>
2024-02-26 20:34:01 -0500

    Fix merge

--- src/constants/nav.js
+++ src/constants/nav.js
@@ -1,6 +1,54 @@
 const navLinks = [
   { name: "Restaurantes", route: "restaurantes" },
-  { name: "Sobre nosotros", route: "sobre" }
-]
+  { name: "Sobre nosotros", route: "sobre" },
+];
 
-export { navLinks }
+const restaurants = [
+  {
+    name: "Restaurante La Barra",
+    image: "barra",
+    direction: "Cl. 6 #79f - 17",
+    category: "Restaurante",
+    page: "https://labarrarestaurante.com/",
+    description:
+      "Este restaurante informal y acogedor ofrece platos colombianos tradicionales, además de cerveza artesanal y cócteles. En su ambiente rústico, con vigas de madera a la vista, se sirven platos como bandeja paisa, ajiaco y arepas.",
+  },
+  {
+    name: "In Situ",
+    image: "situ",
+    direction: "Cra. 11 #93A-43",
+    category: "Restaurante, Bar de vinos",
+    page: "https://in-situ.com/",
+    description:
+      "Este restaurante elegante, con un ambiente íntimo y moderno, ofrece un menú degustación de alta cocina colombiana con un toque creativo. Su chef, Juan Manuel Barrientos, utiliza ingredientes locales frescos y de temporada para crear platos únicos y sorprendentes.",
+  },
+  {
+    name: "Leo Cocina y Cava",
+    image: "leo",
+    direction: "Cl. 70 #4-69",
+    category: "Restaurante, Bar de vinos",
+    page: "https://restauranteleo.com/",
+    description:
+      "Este restaurante, ubicado en una hermosa casa colonial, ofrece un menú de cocina colombiana moderna con un toque internacional. El chef, Leonardo Espinosa, se especializa en platos elaborados con ingredientes frescos y de temporada, y su carta de vinos ofrece una amplia selección de vinos nacionales e internacionales.",
+  },
+  {
+    name: "Andrés Carne de Res",
+    image: "andres",
+    direction: "Cra. 4 #69A-52",
+    category: "Restaurante, Bar",
+    page: "https://www.andrescarnederes.com/",
+    description:
+      "Este restaurante, famoso por su ambiente fiestero y su decoración extravagante, ofrece una amplia variedad de platos colombianos, incluyendo carnes a la parrilla, mariscos y platos típicos de la región. También cuenta con un bar con música en vivo y una pista de baile.",
+  },
+  {
+    name: "El Chato",
+    image: "chato",
+    direction: "Cl. 23 #6-41",
+    category: "Restaurante",
+    page: "https://pacoelchato.org/",
+    description:
+      "Este restaurante familiar, con un ambiente tradicional y acogedor, ofrece platos colombianos caseros a precios muy razonables. Su especialidad es la bandeja paisa, un plato típico colombiano que incluye carne de res, chorizo, morcilla, chicharrón, arepas, aguacate y frijoles.",
+  },
+];
+
+export { navLinks, restaurants };
